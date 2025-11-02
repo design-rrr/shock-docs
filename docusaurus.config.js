d3b7e76c1191766bbb650963d792919acaa70a29
D <207247077+design-rrr@users.noreply.github.com>
2025-11-02 17:20:07 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/logo.png',
 
   // Set the production url of your site here
-  url: 'shock-docs.vercel.app',
+  url: 'https://shock-docs.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
